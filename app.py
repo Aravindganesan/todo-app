4e9281ec9e08e0b58bf9f62185fa99bdcf5104db
Aravind Ganesan <ganesanaravind@gmail.com>
2026-06-26 22:16:49 -0400

    docker implementation

--- app.py
+++ app.py
@@ -280,4 +280,4 @@ def delete_todo(todo_id):
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", "5000"))
-    app.run(host="127.0.0.1", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False)
